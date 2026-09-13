f9cc810baf46675cd38b5443586c3ccc3824cc92
jaimotalaflare <nicojaime.m@gmail.com>
2026-09-13 11:31:23 -0300

    Release Crystal Duel v0.3: Santuario del Umbral with mobile fantasy board and offline assets

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 const PREFIX='crystal-duel:'+self.registration.scope;
-const CACHE=PREFIX+'v0.2.1';
-const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE=PREFIX+'v0.3.0';
+const FILES=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./v03/styles.css?v=0.3.2", "./v03/game.js", "./v03/cards.js", "./v03/art.js", "./v03/engine.js", "./v03/audio.js", "./v03/assets/arena.webp", "./v03/assets/mages.webp", "./v03/assets/creatures.webp"];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(PREFIX)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
